4da83186859db4daadf28165f62cd6b21f2a22d7
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2023-01-09 15:08:49 -0400

    Played and experimented with structs

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -8,13 +8,51 @@ namespace Structs
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //structs
+        struct Vector2
         {
-            Console.WriteLine("Hello Structs!");
+            public int x;
+            public int y;
+        }
 
-            Console.WriteLine("Press any key to exit");
+        struct Entity
+        {
+            public string name;
+            public int health;
+            public int healthMax;
+            public int damage;
+            public Vector2 position;
+        }
+
+        static Vector2 position;
+        static Entity player;
+
+        static void Main(string[] args)
+        { 
+            player.name = "Baymax";
+            player.health = 10;
+            player.healthMax = 10;
+            player.damage = 5;
+            player.position.x = 2;
+            player.position.y = 3;
+
+            Console.WriteLine("Name: " + player.name);
+            Console.WriteLine("Health: " + player.health + "/" + player.healthMax);
+            Console.WriteLine("Damage: " + player.damage);
+            Console.WriteLine("Position: " + player.position.x + " - " + player.position.y);
+          
+            Console.ReadKey(true);
+
+            Console.WriteLine("You leveled up! Health Max increased by 5!");
+            player.healthMax = player.healthMax + 5;
+
+            Console.WriteLine("Name: " + player.name);
+            Console.WriteLine("Health: " + player.health + "/" + player.healthMax);
+            Console.WriteLine("Damage: " + player.damage);
+            Console.WriteLine("Position: " + player.position.x + " - " + player.position.y);
 
             Console.ReadKey(true);
+
         }
     }
 }
