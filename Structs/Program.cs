495b0e4d7f3fb9bb008030cb7228102deba80d8a
ctschnurr <113189402+ctschnurr@users.noreply.github.com>
2023-01-09 14:13:13 -0400

    Initial commit

---
+++ Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Structs!");
+
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey(true);
+        }
+    }
+}
